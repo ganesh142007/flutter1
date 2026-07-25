806fbbe7ce28326ca62c7ad06bd6ac43662015fd
ganesh07 <vattisivaganeshreddy@gmail.com>
2026-07-25 12:09:15 +0530

    Add files via upload

---
+++ exp1.dart
@@ -0,0 +1,12 @@
+void runApp(MyApp myApp) {
+}
+
+class MyApp extends StatelessWidget {
+const MyApp({super.key});
+
+@override
+Widget build(BuildContext context) {
+return MaterialApp(
+title: 'Flutter Demo',
+    }
+}
